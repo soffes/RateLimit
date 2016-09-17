834b0c4436d8d5f57ecbcc234f9c6b97293e0e90
Sam Soffes <sam@soff.es>
2016-09-17 11:11:40 +0200

    Update tests to Swift 3

--- Tests/RateLimitTests.swift
+++ Tests/RateLimitTests.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  RateLimitTests.swift
 //  RateLimitTests
@@ -15,12 +17,12 @@ class RateLimitTests: XCTestCase {
         let name = "testLimit"
 
         // It should get excuted first
-        let expectation1 = expectationWithDescription("Execute 1")
+        let expectation1 = expectation(description: "Execute 1")
         var reported = RateLimit.execute(name: name, limit: 2) {
             expectation1.fulfill()
         }
         XCTAssertTrue(reported)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
 
         // Not right away after
         reported = RateLimit.execute(name: name, limit: 1) {
@@ -32,24 +34,24 @@ class RateLimitTests: XCTestCase {
 		sleep(1)
 
         // Now it should get executed
-        let expectation2 = expectationWithDescription("Execute 2")
+        let expectation2 = expectation(description: "Execute 2")
         reported = RateLimit.execute(name: name, limit: 1) {
             expectation2.fulfill()
         }
         XCTAssertTrue(reported)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
     }
 
     func testResetting() {
         let name = "testResetting"
 
         // It should get excuted first
-        let expectation1 = expectationWithDescription("Execute 1")
+        let expectation1 = expectation(description: "Execute 1")
         let reported1 = RateLimit.execute(name: name, limit: 1) {
             expectation1.fulfill()
         }
         XCTAssertTrue(reported1)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
 
         // Not right away after
         let reported2 = RateLimit.execute(name: name, limit: 1) {
@@ -61,25 +63,25 @@ class RateLimitTests: XCTestCase {
         RateLimit.resetLimitForName(name)
 
         // Now it should get executed
-        let expectation2 = expectationWithDescription("Execute 2")
+        let expectation2 = expectation(description: "Execute 2")
         let reported3 = RateLimit.execute(name: name, limit: 1) {
             expectation2.fulfill()
         }
         XCTAssertTrue(reported3)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
     }
 
 	func testResettingAll() {
         // It should get excuted first
 		let name1 = "TestResettingAll1"
-		let expecation1 = expectationWithDescription("Execute 1")
+		let expecation1 = expectation(description: "Execute 1")
 		let reported1 = RateLimit.execute(name: name1, limit: 1) {
 			expecation1.fulfill()
 		}
 		XCTAssertTrue(reported1)
 
 		let name2 = "TestResettingAll2"
-		let expecation2 = expectationWithDescription("Execute 2")
+		let expecation2 = expectation(description: "Execute 2")
 		let reported2 = RateLimit.execute(name: name2, limit: 1) {
             expecation2.fulfill()
         }
@@ -100,18 +102,18 @@ class RateLimitTests: XCTestCase {
         RateLimit.resetAllLimits()
 
         // Now it should get executed
-		let expectation3 = expectationWithDescription("Execute 3")
+		let expectation3 = expectation(description: "Execute 3")
 		let reported5 = RateLimit.execute(name: name1, limit: 1) {
             expectation3.fulfill()
         }
         XCTAssertTrue(reported5)
 
-        let expectation4 = expectationWithDescription("Execute 4")
+        let expectation4 = expectation(description: "Execute 4")
 		let reported6 = RateLimit.execute(name: name2, limit: 1) {
             expectation4.fulfill()
         }
         XCTAssertTrue(reported6)
 
-		waitForExpectationsWithTimeout(1, handler: nil)
+		waitForExpectations(timeout: 1, handler: nil)
     }
 }
